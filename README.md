8bf4d44ab7879bb858d5f0d87494b5b94e7c205c
chrisbrasington <chrisbrasington@gmail.com>
2014-09-13 19:04:36 -0600

    first iteration wrapper

---
+++ README.md
@@ -0,0 +1,51 @@
+##guildwars2-javascript-api-wrapper
+
+=========
+Simple Javascript Library of Functions for the [Guild Wars 2 public API](http://wiki.guildwars2.com/wiki/API:Main). 
+
+Requirements
+------------------
+* **jQuery 1.8.0+** (Example using jQuery 2.1.1 hosted on Google's servers)
+
+###Dynamic Events 
+* [GetEventNames](http://wiki.guildwars2.com/wiki/API:1/event_namess) - This resource returns an unordered list of the localized event names for the specified language.
+* [GetMapNames](http://wiki.guildwars2.com/wiki/API:1/map_names) - This resource returns an unordered list of the localized map names for the specified language. Only maps with events are listed. 
+* [GetEventDetails](http://wiki.guildwars2.com/wiki/API:1/event_details) - This resource returns static details about available events. 
+* [GetGuildDetails](http://wiki.guildwars2.com/wiki/API:1/guild_details) - This resource returns a details about a guild. 
+
+###Item and Recipe 
+* [GetItems](http://wiki.guildwars2.com/wiki/API:1/items) - This resource returns a list of items that were discovered by players in the game. Details about a single item can be obtained using the item details resource. 
+* [GetItemDetails](http://wiki.guildwars2.com/wiki/API:1/item_details) - This resource returns a details about a single item. 
+* [GetRecipes](http://wiki.guildwars2.com/wiki/API:1/recipes) - This resource returns a list of recipes that were discovered by players in the game. Details about a single recipe can be obtained using the recipe details resource. 
+* [GetRecipeDetails](http://wiki.guildwars2.com/wiki/API:1/recipe_details) - This resource returns a details about a single item. 
+* [GetSkins](http://wiki.guildwars2.com/wiki/API:1/skins) - This resource returns a list of skins that are available in the game. Details about a single skin can be obtained using the skin details resource. 
+* [GetSkinDetails](http://wiki.guildwars2.com/wiki/API:1/skin_details) - This resource returns details about a single skin. 
+* [GetDyes](http://wiki.guildwars2.com/wiki/API:1/colors) - This resource returns all dyes in the game, including localized names and their color component information. 
+ 
+
+###Maps
+* [GetContinents](http://wiki.guildwars2.com/wiki/API:1/continents) - This resource returns static information about the continents used with the map floor resource. 
+* [GetMaps](http://wiki.guildwars2.com/wiki/API:1/maps) - This resource returns details about maps in the game, including details about floor and translation data on how to translate between world coordinates and map coordinates. 
+* [GetMapsDetails](http://wiki.guildwars2.com/wiki/API:1/maps) - This resource returns details about a single map.
+* [GetMapFloor](http://wiki.guildwars2.com/wiki/API:1/map_floor) - This resource returns details about a map floor, used to populate a world map. All coordinates are map coordinates. The returned data only contains static content. Dynamic content, such as vendors, is not currently available. 
+ 
+### World Versus World
+* [GetWVWMatches]() - This resource returns a list of the currently running WvW matches, with the participating worlds included in the result. Further details about a match can be requested using the wvw match details resource. 
+* [GetWVWMatchesDetails](http://wiki.guildwars2.com/wiki/API:1/wvw/match_details) - This resource returns further details about the specified match, including the total score and further details for each map. 
+* [GetWVWObjectiveNames](http://wiki.guildwars2.com/wiki/API:1/wvw/objective_names) - This resource returns an unordered list of the localized WvW objective names for the specified language. 
+
+###Commerce
+* [GetExchange](http://wiki.guildwars2.com/wiki/API:2/commerce/exchange) - This resource returns a list of accepted resources for the gem exchange. 
+* [GetExchangeCoins](http://wiki.guildwars2.com/wiki/API:2/commerce/exchange/coins) - This resource returns the current coins to gems exchange rate. 
+* [GetExchangeGems](http://wiki.guildwars2.com/wiki/API:2/commerce/exchange/gems) - This resource returns the current gem to coins exchange rate. 
+* [GetCommerceListings](http://wiki.guildwars2.com/wiki/API:2/commerce/listings) - This resource returns current buy and sell listings from the trading post. 
+* [GetCommercePricings](http://wiki.guildwars2.com/wiki/API:2/commerce/prices) - This resource returns current aggregated buy and sell listing information from the trading post. 
+
+### Misc API
+* [GetBuild](http://wiki.guildwars2.com/wiki/API:1/build) - Build Version
+* [GetFiles](http://wiki.guildwars2.com/wiki/API:1/files) - This resource returns commonly requested in-game assets that may be used to enhance API-derived applications. The returned information can be used with the render service to retrieve assets. 
+* [RenderService](http://wiki.guildwars2.com/wiki/API:Tile_service) - The render service provides access to in-game assets like item icons and tiles. 
+
+###Quaggans
+* [GetQuaggans](http://wiki.guildwars2.com/wiki/API:2/quaggans) - This resource returns quaggans. 
+* [GetQuagganImage](http://wiki.guildwars2.com/wiki/API:2/quaggans) - This resource returns quaggan images. 
---
+++ src/guildwars2_api.js
@@ -0,0 +1,327 @@
+// log to console
+function log(message)
+{
+	console.log(message);
+}
+
+// get JSON from url
+function getJSON(url)
+{
+	var data;
+	$.ajax({
+		url:url,
+		type:'GET',
+		dataType: 'json',
+		async: false,
+		success:function(response)
+		{
+			data = response;
+		},
+		failure:failureCallback
+	});
+	return data;
+}
+
+// JSON failure response 
+function failureCallback(object)
+{
+	log('error with getting JSON request');
+}
+
+// guild wars 2 api, dynamic API URL building
+// verison 1 and 2
+function apiCall(apiCallFunction,parameters,version)
+{
+	url = 'https://api.guildwars2.com/';
+	
+	// assume version 1 unless specified
+	if(typeof version === 'undefined')
+	{
+		url += 'v1/';
+	}
+	else
+	{
+		url += 'v2/';
+	}
+	
+	// function call
+	url += apiCallFunction + '.json';
+	
+	// parameters
+	if(parameters)
+	{
+		count = 0;
+		$.each(parameters, function(key,value)
+		{
+			if(count==0)
+			{
+				url += '?';
+				count = count + 1;
+			}
+			else
+			{
+				url += '&';
+			}
+			url += key+'='+value;
+		});
+		
+	}
+	log(url);
+	return getJSON(url);
+}
+
+// Get Events (depricated)
+// http://wiki.guildwars2.com/wiki/API:1/events
+
+// Get Event Names
+// http://wiki.guildwars2.com/wiki/API:1/event_names
+function GetEventNames()
+{
+	return apiCall('event_names');
+}
+
+// Get Map Names
+// http://wiki.guildwars2.com/wiki/API:1/map_names
+function GetMapNames()
+{
+	return apiCall('map_names');
+}
+
+// Get World Names (depircated)
+// http://wiki.guildwars2.com/wiki/API:1/world_names
+
+// Get Event(s) Details
+// http://wiki.guildwars2.com/wiki/API:1/event_details
+function GetEventDetails()
+{
+	return apiCall('event_details');
+}
+
+// Event Details Recalculation
+// http://wiki.guildwars2.com/wiki/API:1/event_details#Coordinate_recalculation
+// https://forum-en.guildwars2.com/forum/community/api/Event-Details-API-location-coordinates/first#post2262330
+
+// Get Guild Details
+// http://wiki.guildwars2.com/wiki/API:1/guild_details
+function GetGuildDetails(guild_id)
+{
+	return apiCall('guild_details',{'guild_id':guild_id});
+}
+
+// Get Items
+// http://wiki.guildwars2.com/wiki/API:1/items
+function GetItems()
+{
+	return apiCall('items');
+}
+
+// Get Item Details
+// http://wiki.guildwars2.com/wiki/API:1/item_details
+function GetItemDetails(item_id)
+{
+	return apiCall('item_details',{'item_id':item_id});
+}
+
+// Get Recipes
+// http://wiki.guildwars2.com/wiki/API:1/recipes
+function GetRecipes()
+{
+	return apiCall('recipes');
+}
+
+// Get Recipe Details
+// http://wiki.guildwars2.com/wiki/API:1/recipe_details
+function GetRecipeDetails(recipe_id)
+{
+	return apiCall('recipe_details',{'recipe_id':recipe_id});
+}
+
+// Get Skins
+// http://wiki.guildwars2.com/wiki/API:1/skins
+function GetSkins()
+{
+	return apiCall('skins');
+}
+
+// Get Skin Details
+// http://wiki.guildwars2.com/wiki/API:1/skin_details
+function GetSkinDetails(skin_id) 
+{
+	return apiCall('skin_details',{'skin_id':skin_id});
+}
+
+// Get Continents
+// http://wiki.guildwars2.com/wiki/API:1/continents
+function GetContinents()
+{
+	return apiCall('continents');
+}
+
+// Get Maps 
+// http://wiki.guildwars2.com/wiki/API:1/maps
+function GetMaps()
+{
+	return apiCall('maps');
+}
+
+// Get Map Details
+// http://wiki.guildwars2.com/wiki/API:1/maps
+function GetMapsDetails(map_id)
+{
+	return apiCall('maps',{'map_id':map_id});
+}
+
+// Get Map Floor
+// http://wiki.guildwars2.com/wiki/API:1/map_floor
+// example: GetMapFloor([1,1]);
+function GetMapFloor(ids)
+{
+	return apiCall('map_floor',{'continent_id':ids[0],'floor':ids[1]});
+}
+
+// Get WVW Matches
+// http://wiki.guildwars2.com/wiki/API:1/wvw/matches
+function GetWVWMatches()
+{
+	return apiCall('wvw/matches');
+}
+
+// Get WVW Match Details
+// http://wiki.guildwars2.com/wiki/API:1/wvw/match_details
+function GetWVWMatchesDetails(match_id)
+{
+	return apiCall('wvw/matches',{'match_id':match_id});
+}
+
+// Get WVW Objective Names
+// http://wiki.guildwars2.com/wiki/API:1/wvw/objective_names
+function GetWVWObjectiveNames()
+{
+	return apiCall('wvw/objective_names');
+}
+
+// Get Build Version
+// http://wiki.guildwars2.com/wiki/API:1/build
+function GetBuild()
+{
+	return apiCall('build');
+}
+
+// Get Dyes/Colors
+// http://wiki.guildwars2.com/wiki/API:1/colors
+function GetDyes()
+{
+	return apiCall('colors');
+}
+
+// Get Files
+// http://wiki.guildwars2.com/wiki/API:1/files
+function GetFiles()
+{
+	return apiCall('files');
+}
+
+// Tile Render Service
+// http://wiki.guildwars2.com/wiki/API:Tile_service
+// http://wiki.guildwars2.com/wiki/API:Render_service
+function RenderService(signature,file_id,format)
+{
+	url = 'https://render.guildwars2.com/file/';
+	url += signature + '/';
+	url += file_id += '.';
+	url += format;
+	log(url);
+	
+	// not implemented downloading of render images, but built the URL
+}
+
+// Get Commerce Exchange
+// http://wiki.guildwars2.com/wiki/API:2/commerce/exchange
+function GetExchange()
+{
+	return apiCall('commerce/exchange',[],'v2');
+}
+
+//Get Commerce Exchange for Coins
+//http://wiki.guildwars2.com/wiki/API:2/commerce/exchange/coins
+function GetExchangeCoins(quantity)
+{
+	return apiCall('commerce/exchange/coins',{'quantity':quantity},'v2');
+}
+
+// Get Commerce Exchange for Gems
+// http://wiki.guildwars2.com/wiki/API:2/commerce/exchange/gems
+function GetExchangeGems(quantity)
+{
+	return apiCall('commerce/exchange/gems',{'quantity':quantity},'v2');
+}
+
+// Get Commerce Listings
+// http://wiki.guildwars2.com/wiki/API:2/commerce/listings
+// example: GetCommerceListings([19684,19709]);
+function GetCommerceListings(ids)
+{
+	return apiCall('commerce/listings',{'ids':ids},'v2');
+}
+
+// Get Commerce Prices 
+// http://wiki.guildwars2.com/wiki/API:2/commerce/prices
+function GetCommercePricings(ids)
+{
+	return apiCall('commerce/prices',{'ids':ids},'v2');
+}
+
+// Get Quaggans
+// http://wiki.guildwars2.com/wiki/API:2/quaggans
+function GetQuaggans()
+{
+	url = 'https://api.guildwars2.com/v2/quaggans';
+	return getJSON(url);
+}
+
+// Get Quaggans Artwork URLs
+// http://wiki.guildwars2.com/wiki/API:2/quaggans
+function GetQuagganImage(image)
+{
+	url = 'https://api.guildwars2.com/v2/quaggans';	
+	url += image;
+	ulr += '.jpg';
+	return getJSON(url);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
